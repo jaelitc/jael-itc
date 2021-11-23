64a913279b5c569dcb26369a81add64076dc6085
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2021-11-23 11:48:44 +0000

    deploy: 43fff91191cd1f89d0130625fbe298c1f6e5585b

--- _next/static/xN1g4KQlDmBkS5D8ApHZA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5a3152fd63d0d5667c51.js"],"/_error":["static/chunks/pages/_error-ada92aa268aa6b6d63ad.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
