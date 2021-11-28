4559d98d11990985bcfd6bd7b96f84b04a9f2e5c
jaelitc <jaelitc@users.noreply.github.com>
2021-11-28 12:42:00 +0000

    deploy: 43fff91191cd1f89d0130625fbe298c1f6e5585b

---
+++ _next/static/sbndpY1zK-6AUG-YcE_0Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f7769822273ee705f927.js"],"/_error":["static/chunks/pages/_error-ada92aa268aa6b6d63ad.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
